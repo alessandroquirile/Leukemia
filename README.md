d51f7a69bd3f7996a3d623b92f28e476e9b0352f
alessandroquirile <a.quirile@studenti.unina.it>
2023-03-04 12:08:48 +0100

    Added further images into dataset/leukemia directory

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Leukemia
-For installing fast all the dependencies, just type: ```pip3 install -r requirements.txt```
+
+For installing fast all the dependencies just type:
+
+```pip3 install -r requirements.txt```
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from utils import create_dataframe, show_image
 
 if __name__ == '__main__':
-    leukemia = "dataset/leukemia"
-    hem = "dataset/healthy"
+    leukemia_dir = "dataset/leukemia"  # 8491 images
+    healthy_dir = "dataset/healthy"  # 3389 images
 
-    df = create_dataframe(leukemia, hem)
-    print(df)  # [0;7272) are tumoral cells
+    df = create_dataframe(leukemia_dir, healthy_dir)
+    print(df)  # [0;8490] are tumoral cells
 
     show_image(df, 1000)
