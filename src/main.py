2d9f9a4b48aecbd5acc6c90cdcbfc0b116f7665d
ivano <ivanomatrisciano@gmail.com>
2023-03-06 20:02:10 +0100

    Added ResNet50 features CSV

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import pandas
+
 from FeatureExtraction.SIFT import SIFT
 from dataframe import create_dataframe, create_features_dataframe, normalize_features
 
@@ -7,9 +9,19 @@ if __name__ == '__main__':
 
     df = create_dataframe(leukemia_dir, healthy_dir, shuffle=False)
 
+    """
     extractor = SIFT()
     features_df = create_features_dataframe(df, extractor=extractor)
+    """
 
+    """
+    compression_opts = dict(method='zip', archive_name='ResNet50_unshuffled_features.csv')  
+    features_df.to_csv('ResNet50_unshuffled_features.zip', index=False, compression=compression_opts)  
+    """
+    features_df = pandas.read_csv("ResNet50_unshuffled_features.zip")
+
+    print(features_df)
     labels = df["leukemia"].to_numpy()
 
     features_df = normalize_features(features_df)
+    print(features_df)
