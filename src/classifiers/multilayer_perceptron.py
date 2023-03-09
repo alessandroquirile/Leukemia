436f2475fb761b61fa0c3a9e60cba8c03d765b79
alessandroquirile <a.quirile@studenti.unina.it>
2023-03-09 16:43:28 +0100

    Improved DNN creation readability

--- src/classifiers/multilayer_perceptron.py
+++ src/classifiers/multilayer_perceptron.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from tensorflow import keras
+from keras import layers
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 
@@ -6,23 +8,23 @@ from sklearn.model_selection import train_test_split
 def train_deep_neural_network(x_train, y_train, plot=False):
     x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2)
 
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Dense(256, input_dim=2048, kernel_initializer='normal', activation='relu'))
-    model.add(tf.keras.layers.Dropout(0.1))
-    model.add(tf.keras.layers.Dense(64, activation='relu'))
-    model.add(tf.keras.layers.Dense(16, activation='relu'))
-    model.add(tf.keras.layers.Dropout(0.1))
-    model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+    model = keras.Sequential(
+        [
+            layers.Dense(256, input_dim=2048, kernel_initializer='normal', activation='relu'),
+            layers.Dropout(0.1),
+            layers.Dense(64, activation='relu'),
+            layers.Dense(16, activation='relu'),
+            layers.Dropout(0.1),
+            layers.Dense(1, activation='sigmoid')
+        ]
+    )
 
     model.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer='adam')
 
     history = model.fit(x_train, y_train, epochs=250, batch_size=16384, verbose=1, validation_data=(x_val, y_val))
 
     if plot:
-        # Show Model's characteristics'
-        print("Characteristics of the model:")
         print(model.summary())
-
         # summarize history for loss
         plt.plot(history.history['loss'])
         plt.plot(history.history['val_loss'])
