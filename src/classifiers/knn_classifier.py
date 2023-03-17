1d8dd36e3b38d95a2afbe7ee4a45b749e7e17230
Alessandro Quirile <57677985+alessandroquirile@users.noreply.github.com>
2023-03-17 11:42:42 +0100

    Update knn_classifier.py

--- src/classifiers/knn_classifier.py
+++ src/classifiers/knn_classifier.py
@@ -4,7 +4,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 def knn(x, y, cv=5):
     param_grid = {"n_neighbors": list(range(5, 30))}
-    grid = GridSearchCV(KNeighborsClassifier(), param_grid, cv=5, n_jobs=-1)
+    grid = GridSearchCV(KNeighborsClassifier(), param_grid, cv=cv, n_jobs=-1)
     grid.fit(x, y)
     model = grid.best_estimator_
     scores = cross_validate(
