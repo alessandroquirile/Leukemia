dea9b727c3dc3ecd0950f73c79b26a6545cf556c
Alessandro Quirile <57677985+alessandroquirile@users.noreply.github.com>
2023-03-17 11:43:13 +0100

    Update svm.py

--- src/classifiers/svm.py
+++ src/classifiers/svm.py
@@ -7,7 +7,7 @@ def svc(x, y, cv=5):
                   'kernel': ['linear', 'rbf', 'poly', 'sigmoid'],
                   'degree': [2, 3, 4],
                   'gamma': ['scale', 'auto']}
-    grid = GridSearchCV(svm.SVC(), param_grid, cv=5, n_jobs=-1)
+    grid = GridSearchCV(svm.SVC(), param_grid, cv=cv, n_jobs=-1)
     grid.fit(x, y)
     model = grid.best_estimator_
     scores = cross_validate(
